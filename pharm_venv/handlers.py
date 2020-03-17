83630e60939cb8ea58e8ec610b06eb67de7acb48
yasashko <vasilenko.alx@gmail.com>
2020-03-17 18:49:12 +0300

    Greetings

--- pharm_venv/handlers.py
+++ pharm_venv/handlers.py
@@ -2,18 +2,26 @@ from bs4 import BeautifulSoup
 from glob import glob
 from random import choice
 import requests
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 import json
+
+from emoji import emojize
+
+from telegram.ext import ConversationHandler
 from telegram import ParseMode
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+
 from telegram import ReplyKeyboardMarkup
 from telegram import ReplyKeyboardRemove
-from telegram.ext import ConversationHandler
+
 from utility import get_keyboard
+from utility import SMILE
+
 
 
 
 def sms(bot, update):
-    print('Кто-то отправил команду /start. Что мне делать?') #вывод сообщения в консоль при отправке команды /start
-    bot.message.reply_text('Здравствуйте, {}! \nНажмите ?'
-                           .format(bot.message.chat.first_name), reply_markup=get_keyboard()) # отправим ответ
+    smile = emojize(choice(SMILE), use_aliases=True)
+    print('Кто-то отправил команду НАЧАТЬ. Что мне делать?') #вывод сообщения в консоль при отправке команды /start
+    bot.message.reply_text('Здравствуйте, {}! \nНажмите'
+                           .format(bot.message.chat.first_name), reply_markup=ReplyKeyboardRemove()) # отправим ответ
     print(bot.message)
